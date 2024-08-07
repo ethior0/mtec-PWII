a56c1270167b9cd0197262655d556216212349a5
ethior0 <thiagomeigmoreira18@gmail.com>
2024-08-07 00:07:22 -0300

    modifica projeto noticias modelo mvc

--- 5. Projeto Notícias Handlebars - 10-06-2024/portal_noticias/controller/index.js
+++ 5. Projeto Notícias Handlebars - 10-06-2024/portal_noticias/controller/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-// import path from "path";
 import ExpressHandlebars from "express-handlebars";
+import { noticias } from "../public/js/noticias.js";
 
 const porta = 2000;
 const app = express();
@@ -8,45 +8,35 @@ const app = express();
 // Pasta dos arquivos estáticos
 app.use(express.static("public"));
 app.use(express.static("node_modules"));
+app.use(express.static("controller"));
+app.use(express.static("model"));
 
 app.engine("handlebars", ExpressHandlebars.engine());
 app.set("view engine", "handlebars");
 
 // Tela inicial
 app.get("/", function (req, res) {
-  // let enderecoHome = path.resolve("./view/index.html");
-  // res.sendFile(enderecoHome);
-  res.render("index");
+  res.render("index", {noticias})
 });
 
 // Notícias
 app.get("/noticiaDs.html", function (req, res) {
-  // let enderecoDs = path.resolve("./view/noticiaDs.html");
-  // res.sendFile(enderecoDs);
   res.render("noticiaDs");
 });
 
 app.get("/noticiaMine.html", function (req, res) {
-  // let enderecoMine = path.resolve("./view/noticiaMine.html");
-  // res.sendFile(enderecoMine);
   res.render("noticiaMine");
 });
 
 app.get("/noticiaEnem.html", function(req, res) {
-  // let enderecoEnem = path.resolve("./view/noticiaEnem.html");
-  // res.sendFile(enderecoEnem);
   res.render("noticiaEnem");
 });
 
 app.get("/noticiaCampus.html", function(req, res) {
-  // let enderecoCampus = path.resolve("./view/noticiaCampus.html");
-  // res.sendFile(enderecoCampus);
   res.render("noticiaCampus");
 });
 
 app.get("/noticiaAdm.html", function(req, res) {
-  // let enderecoAdm = path.resolve("./view/noticiaAdm.html");
-  // res.sendFile(enderecoAdm);
   res.render("noticiaAdm");
 });
 
---
+++ 5. Projeto Notícias Handlebars - 10-06-2024/portal_noticias/public/js/noticias.js
@@ -0,0 +1,47 @@
+export const noticias = [
+    {
+        "id": 1,
+        "titulo": "2º DS quebrou tudo na SPF?",
+        "legendaCard": "Entenda o caso",
+        "imgCardSrc": "img/imgSpf01.jpeg",
+        "linkNoticia": "./noticiaDs.html",
+        "descNoticia": "Recentemente foi época de Semana Paulo Freire e pela 1ª vez dentro da ETEC e DS já veio abrindo um buraco no palco.",
+        "dataPubli": "Publicado em: 09/05/2024 10:22",
+    },
+    {
+        "id": 2,
+        "titulo": "Criado novo servidor do Mine na ETEC!",
+        "legendaCard": "Entenda como entrar",
+        "imgCardSrc": "img/imgMine.avif",
+        "linkNoticia": "./noticiaMine.html",
+        "descNoticia": "O que acha de um Minezinho depois de 6 aulas? É sua oportunidade.",
+        "dataPubli": "Publicado em: 26/05/2024 18h58",
+    },
+    {
+        "id": 3,
+        "titulo": "Já se inscreveu no ENEM 2024? Corre!",
+        "legendaCard": "As inscrições estão abertas e a oportunidade de garantir seu futuro acadêmico espera por você.",
+        "imgCardSrc": "img/imgEnem.webp",
+        "linkNoticia": "./noticiaEnem.html",
+        "descNoticia": "As inscrições estão abertas e a oportunidade de garantir seu futuro acadêmico espera por você.",
+        "dataPubli": "Publicado em: 02/06/2024 10h29",
+    },
+    {
+        "id": 4,
+        "titulo": "Cursos de Tecnologia vão pra maior feira tecnológica da América Latina!",
+        "legendaCard": "Uma seleção diversificada de cursos, desde programação até inteligência artificial, estará em destaque neste evento imperdível.",
+        "imgCardSrc": "img/imgCampus.webp",
+        "linkNoticia": "./noticiaCampus.html",
+        "descNoticia": "Uma seleção diversificada de cursos, desde programação até inteligência artificial, estará em destaque neste evento imperdível.",
+        "dataPubli": "Publicado em: 01/06/2024 12h40",
+    },
+    {
+        "id": 5,
+        "titulo": "A preocupante falta de professores na ETEC",
+        "legendaCard": "Essa situação crítica tem impacto direto na qualidade do ensino e na formação profissional dos estudantes, que são o futuro da economia do estado.",
+        "imgCardSrc": "img/imgFaltaDeProfessor.jpeg",
+        "linkNoticia": "./noticiaAdm.html",
+        "descNoticia": "Essa situação crítica tem impacto direto na qualidade do ensino e na formação profissional dos estudantes, que são o futuro da economia do estado.",
+        "dataPubli": "Publicado em: 03/06/2024 19:49",
+    }
+]
