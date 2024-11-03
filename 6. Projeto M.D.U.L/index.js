bae8c16a0eceddf6d2fe6a670c91b65541700588
thiago.dsm <thiagomeigmoreira18@gmail.com>
2024-11-02 21:43:02 -0300

    adiciona estrutura do projeto

---
+++ 6. Projeto M.D.U.L/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import ExpressHandlebars from "express-handlebars";
+
+const porta = 3000;
+const app = express();
+
+// Pastas estáticas
+app.use(express.static("public/"));
+app.use(express.static("node_modules/"));
+
+app.engine("handlebars", ExpressHandlebars.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(porta, () => {
+  console.log(`Rodando na porta ${porta}`)
+});
